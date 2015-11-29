93a7349005f0374d7d19ee09150ef888040e1c0e
shmoh <shmohamud@email.wm.edu>
2015-11-29 18:07:44 -0500

    first commit

---
+++ morningmotive1.py
@@ -0,0 +1,32 @@
+import time
+
+
+def morningMotive():
+    print('Welcome To Morning Motive')
+    time.sleep(2)
+    print('"Do not go where the path may lead, go instead where there is no path and leave a trail." - Emerson')
+    time.sleep(5)
+    print('"To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment" - Emerson')
+    time.sleep(3)
+    print('"Write it on your heart that every day is the best day in the year" - Emerson')
+    time.sleep(3)
+    print('"For every minute you remain angry, you give up sixty seconds of peace of mind" - Emerson')
+    time.sleep(3)
+    print('"Our greatest glory is not in never failing, but in rising up every time we fail" - Emerson')
+    
+
+#str userChoice = 'y'
+
+#while(userChoice=='y'):
+
+ #   morningMotive()
+
+
+#print('Enter "y" or "n" to run the app again or stop running')
+#userChoice == input()
+
+
+          
+
+          
+    
